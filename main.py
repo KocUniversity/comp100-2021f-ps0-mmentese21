5c8bbdfa53fda323e51f5ee2c7867a381f36e5f6
mmentese21 <mmentese21@ku.edu.tr>
2021-10-01 16:47:38 +0300

    fixed

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 x = int(input('Enter number x: '))
 y = int(input('Enter number y: '))
 print('x^y is equal to ', x**y)
-print('log(x) is ', math.log(x,10))
+print('log(x) is ', math.log(x,2))
 print(79670)
